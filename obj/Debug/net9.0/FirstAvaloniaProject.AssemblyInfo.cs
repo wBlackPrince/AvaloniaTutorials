a2f2d2420ce3ca0e3c7684be76a3a8510c5ee346
wBlackPrince <masteryoda021@gmail.com>
2025-10-19 20:07:07 +0300

    Добавил иконки и новые шрифты на страницу

--- obj/Debug/net9.0/FirstAvaloniaProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/FirstAvaloniaProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstAvaloniaProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db44cfba56f61d7b9d66ebd438652096238b1547")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstAvaloniaProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstAvaloniaProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
